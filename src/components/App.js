c203d1486de718f3bda0838f04f7c7c5e91aac04
kelseamc <kelsea.mcall@gmail.com>
2021-01-11 15:43:22 -0500

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+
 function NavBar() {
   return (
     <nav>
@@ -16,12 +17,20 @@ function Home() {
   );
 }
 
+function About(){
+  return(
+    <div id="about">
+    <h1>About</h1>
+  </div>
+  )
+}
+
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* write an <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
